681248a42534c607b7326251f3ab9d142fbab2f7
lokeshreddy_madupu <130545654+LOkeshreddy047@users.noreply.github.com>
2025-04-27 20:49:03 +0530

    Update Register.js

--- Management/frontend/src/components/Register.js
+++ Management/frontend/src/components/Register.js
@@ -36,7 +36,7 @@ function Register() {
     setSubmitStatus("");
     try {
       const response = await axios.post(
-        "http://localhost:5000/auth/register",
+        "https://management1-55j2.onrender.com/auth/register",
         {
           username: registerData.username,
           password: registerData.password,
